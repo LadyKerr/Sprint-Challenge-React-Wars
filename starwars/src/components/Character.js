ccf5a382669b5c07091bc102f96d9975fd892528
LadyKerr <kadiekerr@gmail.com>
2019-05-31 12:28:15 -0400

    changes didnt push. MVP completed

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,10 +2,11 @@ import React from "react";
 
 const Character = props =>{
   return (
-    <div>
-      <h1>Name: {props.characters.name}</h1>
+    <div className="character">
+      <h1>{props.characters.name}</h1>
       <p>BirthYear: {props.characters.birth_year}</p>
       <p>Gender: {props.characters.gender}</p>
+      <p>Mass: {props.characters.mass}</p>
     </div>
   );
 }
