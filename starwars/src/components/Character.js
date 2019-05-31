d444eeb4ab7f04957cf93859e32e4a8902b0f921
LadyKerr <kadiekerr@gmail.com>
2019-05-31 11:32:52 -0400

    ensure elements render on screen

---
+++ starwars/src/components/Character.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Character = () =>{
+  return (
+    <div>
+      <h1>I am the Character</h1>
+    </div>
+  );
+}
+
+export default Character;
---
+++ starwars/src/components/CharacterList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Character from "./Character";
+
+const CharacterList = () => {
+  return (
+    <div>
+      <h1>I am Character List!</h1>
+      <Character />
+    </div>
+  );
+}
+
+
+export default CharacterList;
